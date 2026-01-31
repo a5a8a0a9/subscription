c73141c895de97b4647707142a441d399ff08378
young <stupid80096@gmail.com>
2026-01-31 19:23:14 +0800

    style: format all

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'yo-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'yo-root',
+	imports: [RouterOutlet],
+	templateUrl: './app.html',
+	styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('subscription');
+	protected readonly title = signal('subscription');
 }
