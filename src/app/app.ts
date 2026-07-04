c1e1367ae9091d95e9c5e1932c31f0563cfc852a
young <stupid80096@gmail.com>
2026-07-04 20:45:01 +0800

    chore: 更新 Angular 版本  v22

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { DatePickerModule } from 'primeng/datepicker';
+import { ButtonDirective } from 'primeng/button';
+import { DatePicker } from 'primeng/datepicker';
 
 @Component({
 	selector: 'yo-root',
-	imports: [RouterOutlet, ButtonModule, DatePickerModule, FormsModule],
+	imports: [RouterOutlet, ButtonDirective, DatePicker],
 	templateUrl: './app.html',
 	styleUrl: './app.scss',
 })
