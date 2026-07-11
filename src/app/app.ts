61c4988c0a1d57d1c2d607d79475d0fb59ae0136
young <stupid80096@gmail.com>
2026-07-11 21:51:53 +0800

    feat: remove theme

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'yo-root',
-	imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule],
+	imports: [],
 	templateUrl: './app.html',
 	styleUrl: './app.scss',
 })
-export class App {
-	date = null;
-	protected readonly title = signal('subscription');
-}
+export class App {}
